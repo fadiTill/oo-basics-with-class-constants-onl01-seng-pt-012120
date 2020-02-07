dc7d233615d387bae4dc3fd32d528963cd6f5eef
fadimata tillman <fadi.tillman@gmail.com>
2020-02-07 17:17:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,18 +13,33 @@
 #
 # end
 #
-class Shoe
-  attr_accessor :color, :size, :material, :condition
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+# 
+# BRANDS = []
+#    def initialize(brand = "nike")
+#    @brand = brand
+#    brand.each.include? do |brand|
+#    BRANDS << brand
+#    end
+#  end
+# 
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+# end
+attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
-BRANDS = []
-   def initialize(brand = "nike")
-   @brand = brand
-   brand.each.include? do |brand|
-   BRANDS << brand
-   end
- end
-
+  BRANDS = []
+  def initialize(brand)
+    @brand = brand
+    if BRANDS.include?(brand) #check if its one of the expected unique brands
+  else
+    BRANDS << brand
+  end
+  end
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
