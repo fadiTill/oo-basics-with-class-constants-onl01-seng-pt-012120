13232717417ce79fe2f7db58ed5232369dd26035
fadimata tillman <fadi.tillman@gmail.com>
2020-02-07 17:12:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,32 @@
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+#
+#   def initialize(brand)
+#     @brand = brand
+#   end
+#
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+#
+# end
+#
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
+BRANDS = []
+   def initialize(brand = "nike")
+   @brand = brand
+   brand.each do |brand|
+   BRANDS << brand
+   end
+ end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
